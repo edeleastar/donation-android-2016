a8b39f994c63fe8cadd0dd659155a43017b1e955
Eamonn de Leastar <edeleastar@wit.ie>
2016-10-19 10:26:02 +0100

    rework donate activity to create donation on service

--- app/src/main/java/app/donation/activity/Donate.java
+++ app/src/main/java/app/donation/activity/Donate.java
@@ -1,23 +1,37 @@
 package app.donation.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import app.donation.R;
 import app.donation.main.DonationApp;
+import app.donation.model.Candidate;
 import app.donation.model.Donation;
 
-public class Donate extends AppCompatActivity
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class Donate extends AppCompatActivity implements Callback<Donation>
 {
   private Button donateButton;
   private RadioGroup paymentMethod;
@@ -28,6 +42,7 @@ public class Donate extends AppCompatActivity
 
   private TextView amountText;
   private TextView amountTotal;
+  private Spinner  candidateSelection;
 
   private DonationApp app;
 
@@ -52,6 +67,9 @@ public class Donate extends AppCompatActivity
     totalDonated = 0;
     target = 10000;
 
+    candidateSelection = (Spinner) findViewById(R.id.spinner);
+    CandidateAdapter adapter = new CandidateAdapter(app.candidates);
+    candidateSelection.setAdapter(adapter);
   }
 
   @Override
@@ -71,11 +89,12 @@ public class Donate extends AppCompatActivity
         donatedAmount = Integer.parseInt(text);
       }
     }
-    if (donatedAmount > 0) {
-      app.newDonation(new Donation(donatedAmount, method));
-      progressBar.setProgress(app.totalDonated);
-      String totalDonatedStr = "$" + app.totalDonated;
-      amountTotal.setText(totalDonatedStr);
+    if (donatedAmount > 0)
+    {
+      Donation donation = new Donation(donatedAmount, method);
+      Candidate candidate = (Candidate) candidateSelection.getSelectedItem();
+      Call<Donation> call = (Call<Donation>) app.donationService.createDonation(candidate._id, donation);
+      call.enqueue(this);
     }
 
     amountText.setText("");
@@ -94,5 +113,63 @@ public class Donate extends AppCompatActivity
         break;
     }
     return true;
+  }
+
+  @Override
+  public void onResponse(Call<Donation> call, Response<Donation> response)
+  {
+    Toast toast = Toast.makeText(this, "Donation Accepteed", Toast.LENGTH_SHORT);
+    toast.show();
+    app.newDonation(response.body());
+    progressBar.setProgress(app.totalDonated);
+    String totalDonatedStr = "$" + app.totalDonated;
+    amountTotal.setText(totalDonatedStr);
+    amountText.setText("");
+    amountPicker.setValue(0);
+  }
+
+  @Override
+  public void onFailure(Call<Donation> call, Throwable t)
+  {
+    Toast toast = Toast.makeText(this, "Error making donation", Toast.LENGTH_LONG);
+    toast.show();
+  }
+
+  private class CandidateAdapter extends BaseAdapter implements SpinnerAdapter {
+    private final List<Candidate> data;
+
+    public CandidateAdapter(List<Candidate> data) {
+      this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+      return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+      return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+      return i;
+    }
+
+    @Override
+    public View getView(int position, View recycle, ViewGroup parent) {
+      TextView text;
+      if (recycle != null) {
+        text = (TextView) recycle;
+      } else {
+        text = (TextView) getLayoutInflater().inflate(
+            android.R.layout.simple_dropdown_item_1line, parent, false
+        );
+      }
+      text.setTextColor(Color.BLACK);
+      text.setText(data.get(position).firstName);
+      return text;
+    }
   }
 }
